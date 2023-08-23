90b332b0d1c50aacfdd848c29b04bf54c5ed200c
hariesh-sambath <shariesh2002@gmail.com>
2023-08-23 21:33:06 +0530

    minor bug fix

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import SpaceBackground from './components/SpaceBackground/SpaceBackground';
-import LandingPage from './pages/LandingPage';
+import LandingPage from './pages/LandingPage/LandingPage';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
