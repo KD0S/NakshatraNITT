97a0d023b75e6649403c3eda1872b06a263f6ca8
Krishnadas Nair <Krishnadas.nair02@gmail.com>
2023-08-23 13:04:19 +0530

    added moon

---
+++ src/components/Moon/Moon.tsx
@@ -0,0 +1,10 @@
+import "./Moon.css"
+
+export default function Moon() {
+    return (
+        <div className="moon-container">
+            <div className="moon"></div>
+            <div className="orbit"></div>
+        </div>
+    )
+}
--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,12 +1,14 @@
 import "./LandingPage.css";
 import Navbar from "../components/Navbar/Navbar";
 import Title from "../components/Title/Title";
+import Moon from "../components/Moon/Moon";
 
 function LandingPage() {
    return (
       <div className="landing-main">
          <Navbar />
          <Title />
+         <Moon />
       </div>
    );
 }
