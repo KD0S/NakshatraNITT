66babcf9f717908c140103c3e1f868b2099ddf81
hariesh-sambath <shariesh2002@gmail.com>
2023-08-23 13:06:29 +0530

    add socials & logo

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,12 +1,16 @@
 import "./LandingPage.css";
 import Navbar from "../components/Navbar/Navbar";
 import Title from "../components/Title/Title";
+import SocialHandles from "../components/SocialHandles/SocialHandles";
 
 function LandingPage() {
    return (
       <div className="landing-main">
          <Navbar />
          <Title />
+         <SocialHandles />
+
+
       </div>
    );
 }
