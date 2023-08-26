78a19223272e9401c2907b5a5b7f3f262b108387
hariesh-sambath <shariesh2002@gmail.com>
2023-08-26 13:42:46 +0530

    fix achievements pg

--- src/pages/Achievements/Achievements.tsx
+++ src/pages/Achievements/Achievements.tsx
@@ -1,20 +1,47 @@
-import "./Achievements.css"
+import "./Achievements.css";
 
 export const Achievements = () => {
-    return (
-        <div id="achievements">
-            <h1>Achievements</h1>
-            <div id="achievements-body">
-                <div className="text-container glassmorphism">
-                    <li>
-                        <ul><b>Nakshatra</b> has  a stellar online presence to cater engrossing astronomy content to space enthusiasts. With more than 200 posts on instagram and Youtube, Nakshatra consistently engaged in quenching the thirst of the astrophiles.</ul>
-                        <ul>Nakshatra conducts intra-college  competitions such as debate, magazine design and JAM, annually. Nakshatra successfully conducted the event Celestial Hunt in <b>INHOTTS’23</b> for the first time, which saw enthusiastic participation from more than a 100 freshers.</ul>
-                        <ul>To take astronomy directly to the student community, Nakshatra also actively collaborated with other organisations such as Pragyan where a stargazing event was conducted specifically for the female students of NITT on the occassion of Women’s Day, and NSS NIT Trichy where demonstrations on optical instruments were shown to more than 120 school students on National Science Day.</ul>
-                        <ul>Nakshatra also conducted a Guest Lecture Series <b>“Spectra”</b> by renowned astrophysicists Dr. Hamsa Padmanabhan and Dr. Mousomi Das. News articles featuring and applauding the club’s activities were published by renowned editorials such as Hindustan Times and The Hindu.</ul>
-                        <ul>The club also boasts of developing an indigenous telescope and its own Twitter Bot.</ul>
-                    </li>
-                </div>
-            </div>
-        </div>
-    )
-}
+   return (
+      <div id="achievements">
+         <h1>Achievements</h1>
+         <ul className="glassmorphism">
+            <li>
+               <b>Nakshatra</b> has a stellar online presence to cater
+               engrossing astronomy content to space enthusiasts. With more than
+               200 posts on instagram and Youtube, Nakshatra consistently
+               engaged in quenching the thirst of the astrophiles.
+            </li>
+            <br></br>
+            <li>
+               Nakshatra conducts intra-college competitions such as debate,
+               magazine design and JAM, annually. Nakshatra successfully
+               conducted the event Celestial Hunt in <b>INHOTTS’23</b> for the
+               first time, which saw enthusiastic participation from more than a
+               100 freshers.
+            </li>
+            <br></br>
+            <li>
+               To take astronomy directly to the student community, Nakshatra
+               also actively collaborated with other organisations such as
+               Pragyan where a stargazing event was conducted specifically for
+               the female students of NITT on the occassion of Women’s Day, and
+               NSS NIT Trichy where demonstrations on optical instruments were
+               shown to more than 120 school students on National Science Day.
+            </li>
+            <br></br>
+            <li>
+               Nakshatra also conducted a Guest Lecture Series <b>“Spectra”</b>{" "}
+               by renowned astrophysicists Dr. Hamsa Padmanabhan and Dr. Mousomi
+               Das. News articles featuring and applauding the club’s activities
+               were published by renowned editorials such as Hindustan Times and
+               The Hindu.
+            </li>
+            <br></br>
+            <li>
+               The club also boasts of developing an indigenous telescope and
+               its own Twitter Bot.
+            </li>
+         </ul>
+      </div>
+   );
+};
