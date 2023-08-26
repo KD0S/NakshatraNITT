134a1c3eb4ebad9f43cddd2a43d1ae18e347e41e
Krishnadas Nair <Krishnadas.nair02@gmail.com>
2023-08-26 19:25:43 +0530

    improved social handles ui

--- src/components/SocialHandles/SocialHandles.tsx
+++ src/components/SocialHandles/SocialHandles.tsx
@@ -11,11 +11,23 @@ import "./SocialHandles.css";
 function SocialHandles() {
    return (
       <div className="socials-main">
-         <FontAwesomeIcon icon={faInstagram} className="socials-icon" />
-         <FontAwesomeIcon icon={faXTwitter} className="socials-icon" />
-         <FontAwesomeIcon icon={faMedium} className="socials-icon" />
-         <FontAwesomeIcon icon={faYoutube} className="socials-icon" />
-         <FontAwesomeIcon icon={faLinkedin} className="socials-icon" />
+         <a href="https://www.instagram.com/nakshatra_nitt/">
+            <FontAwesomeIcon icon={faInstagram}
+               className="socials-icon" />
+         </a>
+         <a href="https://twitter.com/NakshatraNITT">
+            <FontAwesomeIcon icon={faXTwitter}
+               className="socials-icon" />
+         </a>
+         <a href="https://medium.com/nakshatra">
+            <FontAwesomeIcon icon={faMedium} className="socials-icon" />
+         </a>
+         <a href="https://www.youtube.com/channel/UCetB13q6XVirl8Ijf8tJToA">
+            <FontAwesomeIcon icon={faYoutube} className="socials-icon" />
+         </a>
+         <a href="https://www.linkedin.com/company/nakshatra-nit-trichy">
+            <FontAwesomeIcon icon={faLinkedin} className="socials-icon" />
+         </a>
       </div>
    );
 }
