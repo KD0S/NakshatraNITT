07206c1fb987712cf6ce84d6bc2329d122de4770
Alok verma <alokverma4237@gmail.com>
2024-07-17 00:18:05 +0530

    Updated Projects & core members

--- src/pages/Members/memberDetails.tsx
+++ src/pages/Members/memberDetails.tsx
@@ -1,34 +1,38 @@
-import dhruva from "../../assets/images/members/dhruva.png"
-import krishnadas from "../../assets/images/members/krishnadas.png"
-import nishanth from "../../assets/images/members/nishanth.png"
-import karna from "../../assets/images/members/karna.png"
-import harshith from "../../assets/images/members/harshith.png"
+// import dhruva from "../../assets/images/members/dhruva.png"
+// import krishnadas from "../../assets/images/members/krishnadas.png"
+// import nishanth from "../../assets/images/members/nishanth.png"
+// import karna from "../../assets/images/members/karna.png"
+// import harshith from "../../assets/images/members/harshith.png"
+import hema from "../../assets/images/members/hema.png"
+import harish from "../../assets/images/members/harish.jpg"
+import keshav from "../../assets/images/members/keshav.jpg"
+import sarayu from "../../assets/images/members/sarayu.jpg"
 
 
 export const membersDetails = [
    {
-      imgPath: krishnadas,
-      name: "Krishnadas Nair",
-      position: `Software Head`,
+      imgPath: hema,
+      name: "Hemadharini",
+      position: `General Secretary`,
    },
    {
-      imgPath: dhruva,
-      name: "Dhruva Rai",
-      position: `Hardware Head`,
-   },
-   {
-      imgPath: harshith,
-      name: "Harshith",
+      imgPath: harish,
+      name: "Harish",
       position: `President`,
    },
    {
-      imgPath: nishanth,
-      name: "Nishanth",
-      position: `General Secretray`,
+      imgPath: keshav,
+      name: "Keshav",
+      position: `Overall Coordinator`,
    },
    {
-      imgPath: karna,
-      name: "Karna Sai Balaji",
+      imgPath: sarayu,
+      name: "Sarayu Malaivani",
       position: `Treasurer`,
    },
+   // {
+   //    imgPath: karna,
+   //    name: "Karna Sai Balaji",
+   //    position: `Treasurer`,
+   // },
 ];
--- src/pages/Projects/Projects.tsx
+++ src/pages/Projects/Projects.tsx
@@ -4,26 +4,30 @@ const Projects = () => {
    return (
       <div id="projects">
          <h1>Our Projects</h1>
+         <span className="projecthardware">Hardware</span>
          <p className="glassmorphism">
-            With the introduction of the technical teams , our combined efforts
-            led us to participate in Sangam, the inter collegiate technical
-            competition of NITT for the first time. Some of the projects and
-            plans which we have developed are as follows :-
+            <b>AstroTrack Navigator</b> : This project aims at autonomously detecting and following colored tracks using color sensors, OpenCV, and a PID controller. Color sensors detect tracks, while a camera captures images processed by OpenCV to identify the track position. The PID controller adjusts navigation for precise movement, showcasing integrated hardware and software for autonomous navigation in robotics and space exploration.
             <br />
             <br />
-            <b>Cubesat</b> : The CubeSat is a miniature based satellite which
-            was planned. The software components included the algorithms for the
-            CubeSat which include LULC, SrGAN algorithm that helps in
-            determining Land use and cover ,as well as scanning of the
-            demographics.
+            <b>Digital Temperature controller</b> : This project aims at developing a digital PID controller with an actuator interface ASIC for temperature control. Xilinx Vivado is used to design a system that integrates sensors, actuators, and digital control methods to stabilize temperatures effectively.
+         </p>
+         <span className="projectsoftware ">Software</span>
+         <p className="glassmorphism">
+            <b>Photometric Redshift estimation</b> : Photometric redshift estimation is much faster and reliable especially for analysing a large number of galaxies when compared to spectroscopic redshifts. This project aims at employing machine learning techniques to automate and improve the accuracy of redshift estimations which allow us to probe for the distances of galaxies as we observe in their past.
             <br />
             <br />
-            <b>PID Controller</b> : The PID Control concept was used in
-            balancing a ball in the middle of a wooden plank with high precision
-            when subjected to external disturbances . This project explains the
-            functioning of Automated Gyroscopes in the International Space
-            Station which help in maintaining a proper orientation with respect
-            to the Earth and Sun.
+            <b>Astronomical object detection</b> : Object detection has been a challenging task when it comes to noisy astronomical images. This project aims at automating and improving the accuracy of object detection using machine learning techniques. Object detection will help astronomers discover and track celestial bodies.
+            <br />
+            <br />
+            <b>High resolution image reconstruction</b> : More often than not, astronomical images lack detail and contrast due to practical limitations in the telescopes used for observations. Certain algorithms can deconvolve and reconstruct images at higher resolutions. This project aims at employing these Super-resolution techniques to recontruct astronomical images.
+         </p>
+         <span className="projecttelescope">Telescope</span>
+         <p className="glassmorphism">
+         
+            <b>18x Refractor</b> :This project aims at building a 500 mm refractor telescope with a 450 mm focal length objective lens and a 25 mm eyepiece. The telescope will be used to observe the moon and the planets. A solar filter will be used for observing the sun safely. The design features a lightweight tube, precise lens mounting, a focuser mechanism, and a stable tripod.
+            <br />
+            <br />
+            <b>Reflector</b> : This project aims at building a larger reflector type telescope for parallel observations with our 10in 1.2 m focal length reflector. Two mirrors (Primary and Secondary) will be used to direct the light to the eyepiece, allowing us to obtain larger magnifications.
          </p>
       </div>
    );
